5445d2847c59713277c1ef8c2fcaf8c66c17c36b
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-08-15 23:55:42 +0700

    feat: Add CRUD operations for partnership management

--- src/routes/partneship.js
+++ src/routes/partneship.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {PrismaClient} = require('@prisma/client');
 const { route } = require('./lpkRoutes');
 const { date } = require('zod');
+const { id } = require('zod/locales');
 
 const router = express.Router()
 const prisma = new PrismaClient();
@@ -42,7 +43,39 @@ router.post('/',async(req, res) => {
 });
 
 router.get('/', async(req,res) => {
+  try{
 
+    const allPartner = await prisma.partnership.findMany();
+    res.json(allPartner);
+  }catch(err){
+    res.status(500).json({error: err.message})
+  }
 })
 
+router.put('/:id', async(req, res) => {
+ try {
+  const data = req.body;
+  const updated = await prisma.partnership.update({
+    where: {id: req.params.id},
+    data,
+  });
+
+  res.json(updated);
+ } catch (err) {
+  res.status(400).json({errors: err.message && err.errors})
+ } 
+})
+
+router.delete('/:id', async(req, res) => {
+  try {
+    await prisma.partnership.delete({
+      where: {id: req.params.id}
+    })
+
+    res.status(200).json({message: "Delete Succesfully"})
+  } catch (err) {
+    res.status(400).json({errors: res.message})
+  }
+})
 module.exports = router;
+
